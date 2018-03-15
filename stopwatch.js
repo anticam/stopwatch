c23972933e579c4798a2ae2db92fbfc1999ab54d
anticam <37422496+anticam@users.noreply.github.com>
2018-03-15 23:05:05 +0100

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,83 @@
+// global variables
+var time = 0.0;
+var running = false;
+var interval;
+var pastimes = [];
+
+
+
+// execute functions here
+
+setUp();
+
+// function definitions
+
+function startTimer(){
+    if (running == false){
+        interval = setInterval(function(){
+            time = time + 0.01;
+            document.getElementById("timer").innerHTML = time.toFixed(2);
+        },10)
+        running = true;
+    }
+}
+
+function startStop(){
+    if(running == false){
+        startTimer();
+    }
+    else{
+        clearInterval(interval);
+        running = false;
+    }
+}
+
+function recordPastTime(){
+    if(running == true){
+        pastimes.push(time);
+        document.getElementById('pasttimes').innerHTML += time.toFixed(2);
+        document.getElementById('pasttimes').innerHTML += "<br>";
+        //document.getElementById('pasttimes').innerHTML = pastimes;
+    }    
+}
+
+function resetTimer(){
+    if(running == true){
+        clearInterval(interval);
+        document.getElementById('pasttimes').innerHTML = "";
+        time = 0;
+        document.getElementById("timer").innerHTML = time;
+        running = false;
+    }
+}
+
+function setUp(){
+
+    document.getElementById('startstop').addEventListener('click',function(){
+            startStop();
+    });
+
+    document.addEventListener('keypress',function(event){
+        var charCode = event.keyCode || event.which;
+        var charStr = String.fromCharCode(charCode);
+        //alert(charStr);
+        if(charStr =='s' || charStr == 'S' ){
+            startStop();
+        }
+        if(charStr == 't' || charStr == 'T'){
+            recordPastTime();
+        }
+        if(charStr == 'r' || charStr == 'R'){
+            resetTimer();
+        }
+    });
+
+    document.getElementById('recordtime').addEventListener('click',function(){
+        recordPastTime();
+    });
+
+    document.getElementById('reset').addEventListener('click',function(){
+        resetTimer();
+    });
+}
+
